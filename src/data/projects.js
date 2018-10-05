b6a9aefc875f36d2031500ce06ea0cec4172e43c
Hunter M. Wells <hunter.m.wells@gmail.com>
2018-10-05 11:10:29 -0700

    verbiage

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const Projects =[
   src: "../programming/hmw.png",
   title: "hunterdigital.media",
   desc: {
-    p:["A website made in Gatsby to utilize as a supplement to my job applications. Includes several custom react components as well as a few tools peppered in for efficiency. Uses GraphQL queries as Gatsby's data layer for image and asset optimization. Several of my own illustrations decorate the landing page.",
+    p:["A website made in Gatsby to utilize as a supplement to my job applications. Includes several custom react components as well as a few third-party components peppered in. Uses GraphQL queries as Gatsby's data layer for image and asset optimization. Several of my own illustrations decorate the landing page.",
     ],
     tech:[
       {name:"Gatsby", link:"https://www.gatsbyjs.org/"},
